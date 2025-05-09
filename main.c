3d1e7015b20252d625d92d2b2df14483027f675d
AndreNobrega125 <107492839+AndreNobrega125@users.noreply.github.com>
2025-05-09 17:48:40 -0300

    Update main.c
    Comecei o código com o menu, nomes e comecei o void finabocci

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+//Integrantes do grupo: André Ayello de Nobrega Rm561754, André Gouveia de Lima RM564219 e Mirella Mascarenhas RM562092
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+int main(){
+setlocale(LC_ALL, "UTF-8");
+char opcao;
+printf("Menu:\n");
+printf("1 - Sequência de Fibonacci");
+printf("2 - Fatoriais"); 
+printf("3 - Verificar Palíndromo.");
+printf("4 - Verificar Substring.");
+scanf("Escolha uma opção (1/2/3/4): ");
 
+void finabocci() {
+  int n; 
+  printf("Digite a quantidade de termos da sequência de Fibonacci (1 a 50):");
+  scanf("%d", &n);
+}
