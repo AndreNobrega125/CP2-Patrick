a0aebdf4d7e9e040d44cd39ab659f18b801a8663
AndreNobrega125 <107492839+AndreNobrega125@users.noreply.github.com>
2025-05-11 17:16:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
-//Integrantes do grupo: André Ayello de Nobrega Rm561754, André Gouveia de Lima RM564219 e Mirella Mascarenhas RM562092
+//Integrantes do grupo: André Ayello de Nobrega RM561754, André Gouveia de Lima RM564219 e Mirella Mascarenhas RM562092
 #include <stdio.h>
 #include <string.h>
-#include <locale.h>
 int main(){
-setlocale(LC_ALL, "UTF-8");
 char opcao;
 printf("Menu:\n");
 printf("1 - Sequência de Fibonacci");
@@ -12,8 +10,20 @@ printf("3 - Verificar Palíndromo.");
 printf("4 - Verificar Substring.");
 scanf("Escolha uma opção (1/2/3/4): ");
 
-void finabocci() {
-  int n; 
-  printf("Digite a quantidade de termos da sequência de Fibonacci (1 a 50):");
-  scanf("%d", &n);
+ switch (opao){ 
+   case '1'
+     //chamar funçÃo para calcular a sequencia de Fibonacci
+     break;
+   case '2'
+     //chamar a função para calcular os fatoriais
+     break;
+   case '3'
+     //chamar a função para verificar palindromos
+     break;
+   case '4'
+     //cahamar a função para verificar substrings
+     break; 
+   default:
+   printf("Opcao invalida. Tente novamente.\n") 
+}
 }
