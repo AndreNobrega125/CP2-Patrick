14d0e4603b2e0f71d6dceed5d0eed3863cb6db47
mimasc <rm562092@fiap.com.br>
2025-05-13 11:37:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Fatoriais() {
+                int N;
+                unsigned long long fat[20];
+                printf("Digite um número inteiro de 1 a 20:");
+                scanf("%d", &N);
+                if (N < 1 || N > 20) {
+                    printf("Valor inválido!\n");
+                    return;
+                }
+                for (int i = 1; i <= N; i++) {
+                    fat[i - 1] = 1;
+                    for (int j = 2; j <= i; j++) {
+                        fat[i - 1] *= j;
+                    }
+                    printf("%d! = %llu\n", i, fat[i - 1]);
+            } }
 int main() {
     char opcao;
     int n;
@@ -44,25 +60,10 @@ int main() {
             printf("\n");
             break;
         }
-        case '2': { 
-            void Fatoriais() {
-                int N;
-                unsigned long long fat[20];
-                printf("Digite um número inteiro de 1 a 20:");
-                scanf("%d", &N);
-                if (N < 1 || N > 20) {
-                    printf("Valor inválido!\n");
-                    return;
-                }
-                for (int i = 1; i <= N; i++) {
-                    fat[i - 1] = 1;
-                    for (int j = 2; j <= i; j++) {
-                        fat[i - 1] *= j;
-                    }
-                    printf("%d! = %||u\n", i, fat[i - 1]);
-                }
+        case '2':  
+            Fatoriais();
             break;
-        }
+        
         case '3': {
             // Código para verificar palíndromo aqui
             break;
@@ -76,4 +77,4 @@ int main() {
     }
     return 0;
     
-} }
+} 
